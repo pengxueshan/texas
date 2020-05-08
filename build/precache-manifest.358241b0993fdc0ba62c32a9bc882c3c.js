d75fe4fb27230185e7f81512d1f0d2f88d7cd694
mountain <mountain@mountaindeMacBook-Pro.local>
2020-05-08 13:11:55 +0800

    build

---
+++ build/precache-manifest.358241b0993fdc0ba62c32a9bc882c3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e51c61ee7170913dc88681a2defdfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a2c7bfea3ff86878a8d",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "b8dfd445bd3742986bed",
+    "url": "/static/css/main.f847eb56.chunk.css"
+  },
+  {
+    "revision": "1a2c7bfea3ff86878a8d",
+    "url": "/static/js/2.d46f5ddf.chunk.js"
+  },
+  {
+    "revision": "ef2381acc2a3a1a43bd541aec43ec571",
+    "url": "/static/js/2.d46f5ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dfd445bd3742986bed",
+    "url": "/static/js/main.65f730c6.chunk.js"
+  },
+  {
+    "revision": "2cf893ad2803c35ce8ad",
+    "url": "/static/js/runtime-main.2908b892.js"
+  }
+]);
